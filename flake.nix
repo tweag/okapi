584e4a76abfe4448e42def2c3641e86da0e0264e
Torsten Schmits <torstenschmits@gmail.com>
2021-08-03 12:38:20 +0200

    set up ocaml deps in flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,13 @@
 {
   description = "Gazelle Extension for OBazl";
 
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/fbfb79400a08bf754e32b4d4fc3f7d8f8055cf94;
-    flake-utils.url = github:numtide/flake-utils;
-    obazl.url = github:tek/rules_ocaml;
-  };
+  inputs.obazl.url = github:tek/rules_ocaml;
 
-  outputs = { nixpkgs, flake-utils, obazl, ... }:
+  outputs = { obazl, ... }:
   let
-    main = system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      test = import nix/test.nix { inherit pkgs; };
-      testInShell = pkgs.writeScript "test" "nix develop -c ${test.test}";
-    in {
-      devShell = (obazl.flakes {}).shell pkgs;
-      apps = {
-        test = {
-          type = "app";
-          program = "${testInShell}";
-        };
-      };
-    };
+    depsOpam = [
+      "codept"
+    ];
 
-  in flake-utils.lib.eachDefaultSystem main;
+  in (obazl.flakes { inherit depsOpam; }).systems;
 }
